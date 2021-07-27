f4a015a0245c328dd4487d8bd263b03217b42714
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-27 13:39:12 -0500

    add password confirmation column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_021839) do
+ActiveRecord::Schema.define(version: 2021_07_27_183753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_07_23_021839) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_confirmation"
   end
 
 end
