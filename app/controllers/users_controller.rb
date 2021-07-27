9df422f4e332a96df95eb8a1d76539dbc94864ca
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-27 13:54:18 -0500

    add happy sad path for users create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,13 @@ class UsersController < ApplicationController
     user = User.new(
       name:params[:name],
       email:params[:email],
-      password_digest: params[:password]
+      password_digest: params[:password],
+      # password_confirmation: params[:password_confirmation]
     )
-    user.save
-
-    render json: user.as_json
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
 end
