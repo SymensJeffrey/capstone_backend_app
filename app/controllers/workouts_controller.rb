0fed75db6e241b3efa04761d14a4ec778f349ba5
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-29 20:17:04 -0500

    remove reference text from workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -20,30 +20,3 @@ class WorkoutsController < ApplicationController
 end
 
 
-
-# def create
-#   carted_products = CartedProduct.where(user_id: current_user.id, status:"carted")
-
-#   calculated_subtotal = 0
-#   calculated_tax = 0
-#   carted_products.each do |cp|
-#     calculated_subtotal += cp.product.price.to_i + cp.quantity.to_i
-#     calculated_tax += cp.product.tax.to_i + cp.quantity.to_i
-#   end
-
-#   order = Order.new(
-#     user_id: current_user.id,
-#     subtotal: calculated_subtotal,
-#     tax: calculated_tax,
-#     total: calculated_subtotal + calculated_tax
-#   )
-#   order.save
-#   render json: order
-
-#   carted_products.each do |cp|
-#     cp.update(status: "purchased", order_id: order.id)
-#   end
-# end   
-
-
-# end
