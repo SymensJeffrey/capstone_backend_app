21ab06dcbc7fec8468a57bf60f6f5b2acd211287
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-29 19:40:37 -0500

    add lift create

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,6 +1,19 @@
 class LiftsController < ApplicationController
 
   def create
-    render json: {message:'hello'}
+    lift = Lift.new(
+      exercise_id:params[:exercise_id],
+      reps:params[:reps],
+      sets:params[:sets],
+      weight:params[:weigth],
+      workout_status:"in progress",
+      user_id:params[:user_id]
+    )
+    if lift.save
+      render json: { message: "Lift added to workout" }, status: :created
+    else
+      render json: { errors: lift.errors.full_messages }, status: :bad_request
+    end
   end
+
 end
